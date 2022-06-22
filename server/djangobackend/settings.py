6c63239ab38a7bb8edad77ca980e9e4e82b42a93
Sundeep220 <sundeepkumarsingh0@gmail.com>
2022-06-22 16:15:48 +0000

    Updated route and allowed host

--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 APPEND_SLASH = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost",
+                 "sundeepk.apic.eu-gb.mybluemix.net"]
 
 
 # Application definition
